632c365d0c144882a89a20e09fd6ae350910fc2a
Radu Crican <radu.crican@workday.com>
2018-03-01 15:52:42 +0100

    Spark Udemy first example with Linear Regression

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "spark-learn"
+
+organization := "catland"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+crossScalaVersions := Seq("2.10.4", "2.11.2")
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+  "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
+  "org.apache.spark" %% "spark-core" % "2.3.0",
+  "org.apache.spark" %% "spark-sql" % "2.3.0",
+  "org.apache.spark" %% "spark-mllib" % "2.3.0"
+)
+
+initialCommands := "import example._"
---
+++ src/main/scala/com/sparklearn/spark/LR.scala
@@ -0,0 +1,67 @@
+package com.sparklearn.spark
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object LR {
+  def main(args: Array[String]): Unit = {
+    import org.apache.spark.SparkConf
+
+    val conf = new SparkConf().setAppName("Your Application Name").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder().appName("Spark Learn").getOrCreate()
+    import spark.implicits._
+
+    val data = spark.read.option("header", "true").option("inferSchema", value = true).format("csv").load("/Users/radu.crican/sources/git/SparkLearn/src/main/resources/USA_Housing.csv")
+
+    printData(data)
+
+    // ("label", "features")
+    val df = data.select(data("Price").as("label"),
+      $"Avg Area Income",
+      $"Avg Area House Age",
+      $"Avg Area Number of Rooms",
+      $"Avg Area Number of Bedrooms",
+      $"Area Population",
+      $"Price")
+
+    val assembler = new VectorAssembler().setInputCols(Array(
+      "Avg Area Income",
+      "Avg Area House Age",
+      "Avg Area Number of Rooms",
+      "Avg Area Number of Bedrooms",
+      "Area Population",
+      "Price")).setOutputCol("features")
+
+    val output = assembler.transform(df).select($"label", $"features")
+
+    val lr = new LinearRegression()
+    val lrModel = lr.fit(output)
+
+    val trainingSummary = lrModel.summary
+    trainingSummary.residuals.show()
+    trainingSummary.predictions.show()
+    println(s"r2: ${trainingSummary.r2}")
+    println(s"r2: ${trainingSummary.rootMeanSquaredError}")
+
+    spark.close()
+  }
+
+  private def printData(data: DataFrame): Unit = {
+    val colNames = data.columns
+    val firstRow = data.head(1)(0)
+    println("\n")
+    println("Example data row: ")
+    for (ind <- Range(1, colNames.length)) {
+      println(colNames(ind))
+      println(firstRow(ind))
+      println("\n")
+    }
+  }
+}
