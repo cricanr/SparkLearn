bae388cb3b562fda79e56114148b86ca6a79f643
Radu Crican <radu.crican@workday.com>
2018-03-16 17:07:48 +0100

    finished model evaluation example

--- src/main/scala/com/sparklearn/spark/TrainGridsEx.scala
+++ src/main/scala/com/sparklearn/spark/TrainGridsEx.scala
@@ -3,7 +3,11 @@ package com.sparklearn.spark
 import com.sparklearn.spark.LR.getClass
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.param.DoubleParam
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.sql.SparkSession
 
 object TrainGridsEx {
@@ -22,9 +26,10 @@ object TrainGridsEx {
     val path = getClass.getResource("/USA_Housing.csv").getPath
     val data = spark.read.option("header", "true").option("inferSchema", "true").format("csv").load(path)
 
-    data.printSchema()
+    import spark.implicits._
 
-    val df = data.select(data("Price").as("label"),
+    val df = data.select(
+      data("Price").as("label"),
       $"Avg Area Income",
       $"Avg Area House Age",
       $"Avg Area Number of Rooms",
@@ -38,8 +43,32 @@ object TrainGridsEx {
       "Avg Area Income",
       "Avg Area House Age",
       "Avg Area Number of Rooms",
-      "Avg Area Number of Bedrooms",
       "Area Population",
-      "Price"))
+      "Price")).setOutputCol("features")
+
+    val output = assembler.transform(df).select($"label", $"features")
+
+    val Array(training, test) = output.select("label", "features").randomSplit(Array(0.7, 0.3), seed = 12345)
+
+    // model
+    val lr = new LinearRegression()
+
+    val doubleParam: DoubleParam = lr.regParam
+    //  FIXME: val paramGrid = new ParamGridBuilder().addGrid(doubleParam, Array(10000, 0,1)).build()
+    val paramGrid = new ParamGridBuilder().build()
+
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(lr)
+      .setEvaluator(new RegressionEvaluator().setMetricName("r2"))
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.8)
+
+    println(s"trainValidationSplit: $trainValidationSplit")
+
+    val model = trainValidationSplit.fit(training)
+
+    println(s"model.validationMetrics: ${model.validationMetrics.foreach(metric => println(s"$metric"))}")
+
+    model.transform(test).select("features", "label", "prediction").show()
   }
 }
